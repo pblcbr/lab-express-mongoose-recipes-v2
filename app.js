4b79ad7297a7e6578b152e51f67b6f3a0f8ef01c
Pablo Cabrera <pablo.cabrera.lopez@gmail.com>
2025-10-21 19:32:59 +0200

    solved

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const logger = require("morgan");
-
+const mongoose = require("mongoose");
 const app = express();
+const Recipe = require("./models/Recipe.model");
+const bodyParser = require("body-parser");
 
 // MIDDLEWARE
 app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
+app.use(bodyParser.json());
+
 
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
-
+mongoose.connect("mongodb://127.0.0.1:27017/express-mongoose-recipes-dev")
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 
 // ROUTES
@@ -23,23 +29,47 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post("/recipes", (req, res) => {
+    const body = req.body;
+    res.json(body);
+});
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get("/recipes", (req, res) => {
+    const body = req.body;
+    Recipe.create(body)
+    .then(res => res.status(201).json(body))
+    .catch(err => res.status(500).json({ error: err}));
+});
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
+app.get("/recipes/:id", (req, res) => {
+    const id = req.params.id;
+    Recipe.findById(id)
+    .then(res => res.status(200).json(res))
+    .catch(err => res.status(500).json({ error: err}));
+});
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
+app.put("/recipes/:id", (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    Recipe.findByIdAndUpdate(id, body)
+    .then(res => res.status(200).json(res))
+    .catch(err => res.status(500).json({ error: err}));
+});
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
-
+app.delete("/recipes/:id", (req, res) => {
+    const id = req.params.id;
+    Recipe.findByIdAndDelete(id)
+    .then(res => res.status(204).json(res))
+    .catch(err => res.status(500).json({ error: err}));
+});
 
 
 // Start the server
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,50 @@
-// Your code here ...
+// Your code here ...title - Type String. It should be required and unique.
+// instructions - Type String. It should be required.
+// level - Type String. It can be one of the following values: Easy Peasy - Amateur Chef - UltraPro Chef (use the enum validator 😉).
+// ingredients - Type Array of Strings - represented as [ String ].
+// image - Type String. Default value: "https://images.media-allrecipes.com/images/75131.jpg".
+// duration - Type Number. The minimum value should be 0.
+// isArchived - Type Boolean. The default value should be false.
+// created - Type Date. By default, today.
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema({
+    title: { 
+        type: String,
+        required: true,
+        unique: true
+    },
+    instructions: {
+        type: String,
+        required: true
+    },
+    level: {
+        type: String,
+        enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+    },
+    ingredients: {
+        type: [String]
+    },
+    image: {
+        type: String,
+        default: "https://images.media-allrecipes.com/images/75131.jpg"
+    },
+    duration: {
+        type: Number,
+        min: 0
+    },
+    isArchived: {
+        type: Boolean,
+        default: false
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+module.exports = Recipe;
